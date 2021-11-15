95d338ee110eeb49b2d083531cd0f5ef5dbaba57
Marco Ricci <mr.ricci79@gmail.com>
2021-11-15 18:04:23 +0000

    new car form init

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,16 +1,28 @@
 class CarsController < ApplicationController
+  before_action :set_car, only: [:show]
   def index
     @cars = Car.all
   end
 
   def show
-    @car = Car.find(params[:id])
+    @booking = Booking.new()
   end
 
   def new
-
+    @car = Car.new()
   end
+
   def create
 
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit(:comment, :movie_id)
   end
 end
