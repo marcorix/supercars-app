832e563f7a5f5410d489cb11d23c1504465625b9
Marco Ricci <mr.ricci79@gmail.com>
2021-11-15 17:21:40 +0000

    show view created

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,16 @@
 class CarsController < ApplicationController
+  def index
+    @cars = Car.all
+  end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def new
+
+  end
+  def create
+
+  end
 end
