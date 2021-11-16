0ac952961bdb6ce68111968dbac8b57f1f9a0779
Marco Ricci <mr.ricci79@gmail.com>
2021-11-16 15:35:20 +0000

    dashboard implemented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   def home
   end
   def dashboard
-    @booking = Booking.where(user: current_user)
+    # @bookings = Booking.where(user: current_user)
+    @user = current_user
   end
 end
