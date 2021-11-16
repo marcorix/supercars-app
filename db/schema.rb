17b44160647ca30cf24dc2b63066cbbc11efc3ae
Leon Wisskirchen <hipwis@web.de>
2021-11-16 11:37:30 +0000

    addded price to cars

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_155408) do
+ActiveRecord::Schema.define(version: 2021_11_16_113231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_155408) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "price"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
