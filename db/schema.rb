16318096c4c7b088bd0e317cc1d187975b664395
Marco Ricci <mr.ricci79@gmail.com>
2021-11-17 14:34:01 +0000

    added location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_110822) do
+ActiveRecord::Schema.define(version: 2021_11_17_142121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2021_11_17_110822) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price"
     t.string "image_url"
+    t.string "location"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
