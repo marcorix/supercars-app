98f43a3470b12f3403c83e797f9c378068121ed8
Neil Pulcyn <neilpulcyn@hotmail.co.uk>
2021-11-19 11:54:04 +0000

    search function fixed

--- app/models/car.rb
+++ app/models/car.rb
@@ -7,7 +7,7 @@ class Car < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   include PgSearch::Model
-  pg_search_scope :search_by_model_and_year__and_color_and_address,
+  pg_search_scope :search_by_model_and_year_and_color_and_address,
     against: [ :model, :year, :color, :address ],
     using: {
       tsearch: { prefix: true } # <-- now `superman batm` will return something!
