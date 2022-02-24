a7b187850758dd23a21f550ecec83c4a4d30de7a
Marco Ricci <mr.ricci79@gmail.com>
2022-02-24 09:13:11 +0000

    added collection

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :cars
   has_many :bookings
+  validates :first_name, :last_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/dashboard', to: 'pages#dashboard'
-  get 'cars/map', to: 'cars#map'
+  get 'dashboard', to: 'pages#dashboard'
+  # get 'cars/map', to: 'cars#map'
   resources :cars do
     resources :bookings, only: [ :create]
+    collection do
+      get :map
+    end
   end
   resources :bookings, only: [ :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
